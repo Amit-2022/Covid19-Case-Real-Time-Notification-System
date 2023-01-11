7bba3dfbbe144639c443fc8dd3aa276936304cd1
Amit <97467310+Amit-2022@users.noreply.github.com>
2023-01-11 13:51:00 +0530

    Add files via upload

---
+++ notification_system/entity/config_entity.py
@@ -0,0 +1,6 @@
+from collections import namedtuple
+
+TemplatesConfig = namedtuple("TemplatesConfig", ["icon_dir",
+                                                 "icon_name"])
+
+NotificationConfig = namedtuple("NotificationConfig", ["covid_data_url"])                                                
